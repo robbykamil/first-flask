fddf12ca77e95257180083d6780807ffa6fe3f39
Robby Kamil <71558401+robbykamil@users.noreply.github.com>
2021-08-27 21:23:36 +0700

    Added some features for camera access

--- main.py
+++ main.py
@@ -1,47 +1,59 @@
+from flask import Flask, request, redirect, url_for, render_template, Response
+import cv2
 import os
-import base64
-import io
+#from numpy.lib.shape_base import expand_dims
+#import tensorflow as tf
+#import numpy as np
 
-#import image
-from PIL import Image
-from flask import Flask, request, render_template, redirect, url_for
-from google.cloud import storage
 
+#from PIL import Image
+#from google.cloud import storage
 
 app = Flask(__name__, template_folder="template")
 
-# Configure this environment variable via app.yaml
-CLOUD_STORAGE_BUCKET = os.environ['CLOUD_STORAGE_BUCKET']
 
-storage_client = storage.Client()
+
+def generate_frames():
+    camera = cv2.VideoCapture(0)
+    while True:
+            
+        ## read the camera frame
+        success,frame=camera.read()
+        if not success:
+            break
+        else:
+            ret,buffer=cv2.imencode('.jpg',frame)
+            frame=buffer.tobytes()
+            yield(b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
 @app.route('/')
-def home():
-    return render_template("index.html")
+def index():
+    return render_template('isyaratku.html')
 
 @app.route('/translate')
 def translate():
-    return render_template("translate.html")
+    return render_template('translate.html')
 
 @app.route('/get-image', methods=['GET', 'POST'])
 def get_image():
     if request.method == 'POST':
         nama = request.form.get("huruf")
-        return redirect(url_for("pict_download", nama=nama))
+        filepath = nama + ".png"
+        URL = "https://storage.googleapis.com/bismillahdetect/{}".format(filepath)
+        return render_template('get-translate.html', nama=nama, pict=URL)
     else:
-        return render_template("get-translate.html")
-    
-@app.route('/get-image/<nama>')
-def pict_download(nama):
-    filepath = nama + ".png"
-    
-    buc = storage_client.get_bucket(CLOUD_STORAGE_BUCKET)
-    blob = buc.blob(filepath)
-    pict = blob.download_as_bytes()
-    
-    return pict
+        redirect(url_for('translate'))
+
+@app.route('/detect')
+def detect():
+    return render_template('detect.html')
+
+
+@app.route('/video')
+def video():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
-
+    app.run(host='127.0.0.1', port=8080, debug=True)
